6ac22a609f6ba97cb6e054b110f21b0ea9b575d9
ISSA HASSAN <hassosas94@gmail.com>
2025-11-03 23:08:10 +0300

    Add files via upload

---
+++ escapesequence.cpp
@@ -0,0 +1,17 @@
+// escape sequence operators. special characters that
+// control the formatting of output on CLI program.
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "starthmore univesity is the leading\n university in the region" << endl;
+    cout << "\t testing the tab oprator" << endl;
+    cout << "\\ testing the back slash operator" << endl;
+    cout << "\' testing the single quote operator'" << endl;
+    cout << "\" testing the double quote operator\"" << endl;
+    cout << "\a testing the alert operator" << endl;
+
+    return 0;
+}
+
+// THIS IS HOW TO SHORT CODE USING ESCAPE SEQUENCE
